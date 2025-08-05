3b0869ae2ff56752b4588791c916cd6f12585e3e
Hyperiongate <jim@shift-work.com>
2025-08-04 18:40:20 -0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,14 @@
 # utils/__init__.py
 # This file makes the utils directory a Python package
+
+"""
+Utils Package for Workforce Scheduler
+
+This package contains helper functions and utilities used throughout the application.
+"""
+
+# You can optionally import commonly used functions here for easier access
+# from .helpers import get_coverage_gaps, get_overtime_opportunities
+
+# For now, we'll keep it simple - just having this file is enough
+# to make Python recognize 'utils' as a package
